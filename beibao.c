6452e53ef334d3329e553ad514abb73d7350dd2b
Frank <songdongjob@163.com>
2021-06-02 20:02:54 +0800

    解决0-1背包问题,未做优化

--- beibao.c
+++ beibao.c
@@ -22,14 +22,19 @@
  *
  * 状态转义方程
  * f(n, W) = {
+ *		//这里n表示下标计数，程序中从0开始计数
  *		n = 1, f(1, W) = Wn > W ? Vn : 0
  *		n = 2, f(2, W) = max(f(1, W), f(1, W-W1) + V1)
  *		...
  *		n = N, f(N, W) = max(f(N-1, W), f(N-1), W-WN + VN)
  * }
  *
- * 定义DP数组：由状态转义方程可以看出
+ * 定义DP数组：由状态转义方程可以看出,需要计算的子问题个数有N*W个，
+ * 也就是要计算有n(0<=n<=N)个物品,限重分别为w(0<=w<=W)的情况下的
+ * 最大价值.
  */
+
+#include <stdio.h>
 
 /**
  * 假设物品的价值和重量都是正整数
@@ -41,5 +46,55 @@ struct thing {
 
 int zero_one_package(struct thing* things_list, int num, int weight_limit)
 {
+	int		i = 0;
+	int		j = 0;
+	int		tmp_limit = 0;
+	int**	dp_array = NULL;
 
+	dp_array = calloc(1, sizeof(void*)*num);
+	for (i = 0; i < num; ++i) {
+		dp_array[i] = calloc(1, sizeof(int)*(weight_limit+1));
+	}
+
+	for (i = 0; i < num; ++i) {					// 物品数量
+		for (j = 1; j <= weight_limit; ++j) {	// 限重范围
+			if (i == 0) {
+				dp_array[0][j] = things_list[0].weight > j ? 0 : things_list[0].value;
+			} else {
+				if (j < things_list[i].weight) {
+					// 当前物品超过限重时, 肯定不能带
+					dp_array[i][j] = dp_array[i-1][j];
+				} else {
+					// 当前物品不超过时, 可带可不带，需要做比较
+					tmp_limit = j - things_list[i].weight;
+					if (tmp_limit <= 0)
+						dp_array[i][j] = dp_array[i-1][j] > things_list[i].value ? 
+											dp_array[i-1][j] : things_list[i].value;
+					else
+						dp_array[i][j] = dp_array[i-1][j] > (dp_array[i-1][tmp_limit] + things_list[i].value) 
+										? dp_array[i-1][j] : (dp_array[i-1][tmp_limit] + things_list[i].value);
+				}
+			}
+			//printf("dp[%d][%d] = %d\r\n", i, j, dp_array[i][j]);
+		}
+	}
+	return dp_array[num-1][weight_limit];
+}
+
+int main(int argc, char*argv[])
+{
+	struct thing thing_list[10] = {
+		{6, 540},
+		{3, 200},
+		{4, 180},
+		{5, 350},
+		{1, 60},
+		{2, 150},
+		{3, 280},
+		{5, 450},
+		{4, 320},
+		{2, 120},
+	};
+	
+	printf("rst = %d.\r\n", zero_one_package(thing_list, 10, 30));
 }
